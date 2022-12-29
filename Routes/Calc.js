bdd585fb8aaf1df613c3306c6460a150d7736d6c
hentorgeman <hen.torgeman6@gmail.com>
2022-12-29 12:11:40 +0200

    Script working with the bounding

--- Routes/Calc.js
+++ Routes/Calc.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Calc = require("../Controllers/Calc");
 
-// router.get("/GetCircels", Calc.GetCircels);
-// router.get("/CreateDirections", Calc.CreateDirections);
-// router.post("/CreateCoCircels", Calc.CreateCoCircels);
-// router.get("/GetCoCircelsDataTest", Calc.GetCoCircelsDataTest);
 router.post("/ClearDB", Calc.ClearDB);
 
 module.exports = router;
